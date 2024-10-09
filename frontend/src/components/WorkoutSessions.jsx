c6e2c0073292d5b6fce8c4946347032a911be7fd
manishsingh0418 <manishkumar80819@gmail.com>
2024-10-09 19:26:30 +0530

    Project Setup and Navbar

---
+++ frontend/src/components/WorkoutSessions.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const WorkoutSession = () => {
+  return (
+    <section className='workout_session'>
+    <div className='wrapper'>
+      <h1>TOP WORKOUT SESSION</h1>
+      <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
+      quisquam. Eaque alias, blanditiis quidem maiores illo unde ipsam!
+      </p>
+      <img src="/img5.jpg" alt="workout"/>
+    </div>
+    <div className="wrapper">
+      
+    </div>
+    </section>
+  )
+}
+
+export default WorkoutSession
