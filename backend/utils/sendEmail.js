d0598d971411c71068873b6990bfa9b1b6c8e49a
manishsingh0418 <manishkumar80819@gmail.com>
2024-10-10 01:35:03 +0530

    Final Commit

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,20 +1,21 @@
 import nodeMailer from "nodemailer";
-export const sendEmail = async(options)=>{
-    const transporter = nodeMailer.nodeTransporter({
-      
-        host:process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        service:process.env.SMTP_SERVICE,
-        auth: {
-          user: process.env.SMTP_MAIL,
-          pass: process.env.SMTP_PASSWORD,
-        },
-    });
-    const mailOptions ={
-        from : nprocess.env.SMTP_MAIL,
-        to:options.mail,
-        subject:options.subject,
-        text:`${options.message} \n\n Email of User Who Sent The Message: ${options.userEmail}`,
-        };
-        await transporter.sendMail(mailOptions);
+
+export const sendEmail = async (options) => {
+  const transporter = nodeMailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    service: process.env.SMTP_SERVICE,
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.SMTP_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: `${options.message} \n\nEmail of User Who Sent The Message: ${options.userEmail}`,
+  };
+  await transporter.sendMail(mailOptions);
 };
